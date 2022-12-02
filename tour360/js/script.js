7660e550597d52c915fc05551c4c4fd0deb0bc38
sotoh1 <70956949+sotoh1@users.noreply.github.com>
2022-12-02 18:20:53 -0500

    Update script.js
    Updated script, added Dropbtn JS

--- tour360/js/script.js
+++ tour360/js/script.js
@@ -1,4 +1,4 @@
-
+/* Navbar Script */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,7 +8,7 @@ function myFunction() {
   }
 }
 
-
+/* Slideshow Script */
 var slideIndex = 0;
 showSlides();
 
@@ -26,11 +26,12 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  /* slides.eq(i).fadeIn(1000); */
+  setTimeout(showSlides, 9000);  // Change image every 2 seconds
 }
 
 
-
+/* Collapsible Script */
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -45,3 +46,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+/* dropbtn Script */
+var coll = document.getElementsByClassName("dropbtn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+/* Hover Tabs Script */
+/* function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} */
